a6eae80f5f90a3a5b3ffc3ec2c4692066d5b164d
LeeSundy <656233622@qq.com>
2016-02-13 22:21:03 +0800

    添加测试代码

--- src/d_test/main/Main.java
+++ src/d_test/main/Main.java
@@ -21,7 +21,16 @@ public class Main implements Runnable
 	@Override
 	public void run()
 	{
-		//填写测试代码
+		/*
+		 * 填写测试代码
+		 */
+		GameCore.setGameTitle("猴赛雷偷桃(￣▽￣)");// 加载标题
+		GameCore.loadBgView("白色.jpg");// 加载背景
+		
+		GameSprite spHouZi = new GameSprite("houzi", 40, 50);// 猴子精灵
+		spHouZi.playSpriteAnimate("tou");// 播放猴子
+		
+		GameCore.pause(99999);//暂停
 	}
 
 	public static void main(String[] args)
