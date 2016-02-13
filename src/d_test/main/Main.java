76b22d05ccc19f8a3dc86646028f50bbf697e131
LeeSundy <656233622@qq.com>
2016-02-13 21:58:04 +0800

    添加截图

--- src/d_test/main/Main.java
+++ src/d_test/main/Main.java
@@ -8,7 +8,6 @@ import java.awt.event.KeyEvent;
 import com.rupeng.game.GameCore;
 import com.sundy.www.gamecore.GameImage;
 import com.sundy.www.gamecore.GameSprite;
-import com.sundy.www.gamecore.GameSpriteCoin;
 import com.sundy.www.gamecore.GameText;
 
 /**
@@ -22,26 +21,7 @@ public class Main implements Runnable
 	@Override
 	public void run()
 	{
-		GameImage im = new GameImage("µ∞µ∞.jpg", 40, 40);
-
-		while (true)
-		{
-			int kc = GameCore.getPressedKeyCode();
-			switch (kc)
-			{
-
-			case KeyEvent.VK_RIGHT:
-				GameCore.setImagePosition(0, GameCore.getImageX(0) + 1,
-						GameCore.getImageY(0)-40);
-				GameCore.showImage(0);
-				break;
-			// case KeyEvent.VK_LEFT:
-			// im.moveLeft();
-			// break;
-			}
-
-		}
-
+		//ÃÓ–¥≤‚ ‘¥˙¬Î
 	}
 
 	public static void main(String[] args)
